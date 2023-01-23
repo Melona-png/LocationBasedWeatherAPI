1083f3c1b7a95c1ead69e720f790abf0660c406f
Melona-png <118491178+Melona-png@users.noreply.github.com>
2023-01-22 21:29:34 -0500

    added input query selector and fixed the click function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,18 @@
-$("btn-info").click(function() {
-});
+var searchFormEl = document.querySelector("#search-form");
 
-console.log("btn-info");
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector("#search-input").value;
+
+  if (!searchInputVal) {
+    console.error("You need a search input value!");
+    return;
+  }
+
+  var queryString = "./search-results.html?q=" + searchInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.addEventListener("submit", handleSearchFormSubmit);
